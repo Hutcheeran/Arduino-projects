0447bda60162e023c98545c85a4ca1edea332714
Hutcheeran <51094530+Hutcheeran@users.noreply.github.com>
2021-01-16 03:04:58 +0100

    Updated TBP-Intro tempo

--- Passive Buzzer/TheBlackParade_Intro.ino
+++ Passive Buzzer/TheBlackParade_Intro.ino
@@ -1,18 +1,22 @@
 
 #include "pitches.h"
 
-// notes in the melody:
+int pin = 8;
+
 int melody[] = {
   NOTE_G5, NOTE_FS5, NOTE_B5, NOTE_E5, NOTE_D5, NOTE_G5, NOTE_C5, NOTE_B4, NOTE_E5, NOTE_A4, NOTE_D5};
 int duration[] = {4, 8, 8, 4, 8, 8, 4, 8, 8, 4, 4};
+//tempo of 88bpm in seconds
+int tempo = 2730;
 
 void setup() {
 }
 
 void loop() {  
   for (int i = 0; i < 11; i++) {
-    int length = 4000/duration[i];
-    tone(8, melody[i], length);
+    int length = tempo/duration[i];
+    tone(pin, melody[i], length);
+
     int pause = length*1.30;
     delay(pause);
   }
