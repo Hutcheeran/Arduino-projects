e6778a1eb58877184318fe60eee5edffbc5da98c
Hutcheeran <51094530+Hutcheeran@users.noreply.github.com>
2021-01-20 13:34:01 +0100

    Edited Digital_Input

--- Digital Input/Digital_input.ino
+++ Digital Input/Digital_input.ino
@@ -1,13 +1,13 @@
-int buttonApin = 9;
-int buttonBpin = 8;
+int buttonApin = 2;
+//int buttonBpin = 8;
 int ledPin = 5;
-int count; 
+boolean state; 
 
 void setup(){
     pinMode(ledPin, OUTPUT);
     pinMode(buttonApin, INPUT_PULLUP); //default value for the switch = HIGH
-    pinMode(buttonBpin, INPUT_PULLUP);
-    count = 0;
+    //pinMode(buttonBpin, INPUT_PULLUP);
+    state = false;
     
 }
 
@@ -32,11 +32,11 @@ void loop(){
     //turns led  on and off from the same button 
     //might need to check how that interacts with loops --- what if I press it multiple times in a row?
     if (digitalRead(buttonApin) == LOW){
-        count++;
+        state = !state;
         delay(500);
     }
 
-    if (count % 2 != 0){
+    if (state){
         digitalWrite(ledPin, HIGH);
     } else {
         digitalWrite(ledPin, LOW);
