ec76b5b0afff6fa18567363ba693c398bf002b24
Hutcheeran <51094530+Hutcheeran@users.noreply.github.com>
2021-01-20 20:08:48 +0100

    testing branch

--- Digital Input/Digital_input.ino
+++ Digital Input/Digital_input.ino
@@ -12,23 +12,6 @@ void setup(){
 }
 
 void loop(){
-    //turn led on with button a, and off with button b
-    /*  if (digitalRead(buttonApin) == LOW){
-    digitalWrite(ledPin, HIGH);
-    }
-
-    if (digitalRead(buttonBpin) == LOW){
-    digitalWrite(ledPin, LOW);
-    } */
-
-
-    //keeps led on HIGH while it's pressed
-    /* if (digitalRead(buttonApin) == LOW) {
-        digitalWrite(ledPin, HIGH); 
-    } else {
-        digitalWrite(ledPin, LOW); 
-    } */
-
     //turns led  on and off from the same button 
     //might need to check how that interacts with loops --- what if I press it multiple times in a row?
     if (digitalRead(buttonApin) == LOW){
